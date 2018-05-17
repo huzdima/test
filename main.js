05c15845c342b41d0168b04776c01b0fc5596b83
huzdima <huz.dima@gmail.com>
2018-05-18 01:36:52 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,152 @@
+// string
+// number
+// object
+// boolean
+// undefined
+// null
+
+var undefined;
+var name = 'Dimma';
+var age = 22;
+
+var isMarried = false;
+var nul = null;
+
+// console.log(name);
+// console.log(age);
+
+
+// var family = {
+//     mother : 'Lubov',
+//     father: "Sasha",
+// };
+// console.log(family.mother);
+
+
+
+
+
+
+// console.log(isMarried);
+// console.log(undefined);
+// console.log(obj);
+
+// console.log(typeof (undefined));
+// console.log(typeof (name));
+// console.log(typeof (age));
+// console.log(typeof (family));
+// console.log(typeof (isMarried));
+// console.log(typeof (nul));
+
+// var blyat = 5;
+// console.log(blyat);
+
+// if(blyat > 0){
+//     console.log('Так блять быльше 0');
+// }else{
+//     console.log('Ні блять менше 0');
+// }
+
+// var name = 'Dimma';
+
+// == , ===
+
+// console.log(a);
+
+// var a = "5";
+// var b = 5;
+//
+// if(a == b){
+//     console.log('True');
+// }else{
+//     console.log('False');
+// }
+
+// if(a === b){
+//     console.log('True');
+// }else{
+//     console.log('False');
+// }
+
+
+// var question = prompt('Введіть назву коляру');
+//
+// // prompt, if/if/if, alert, ===
+//  if(question == 'зелений'){
+//      alert('ыди')
+//  }
+// if(question == 'жовтий'){
+//     alert('чекай')
+// }
+// if(question == 'червоний'){
+//     alert('стый')
+// }
+
+
+// var ololo = prompt('Рохан курить траву?');
+// if (ololo == 'Так') {
+//     confirm('True')
+// } else {
+//     confirm('NE PIZDI')
+// }
+
+// var console = {
+// //     name: 'Rohan',
+// //     age: 21,
+// //     kurwa: false,
+// //     animals: {
+// //         cat: 'Markis',
+// //         dog: 'Berta'
+// //     }
+// // };
+// //  console.log(info.animals.dog);
+//
+// console.log('1');
+// console.log('2');
+// console.log('3');
+// console.log('4');
+// console.log('5');
+
+
+// var mass = ["name", '2sdasdada', true, 2121, undefined, null, "dimmaaaa", 212122];
+//
+// // console.log(mass[0]);
+// // console.log(mass[1]);
+// // console.log(mass[2]);
+// // console.log(mass[3]);
+// // console.log(mass[4]);
+// // console.log(mass[5]);
+// // console.log(mass[6]);
+//
+// console.log(mass[2]);
+//
+//
+//
+// for (var i = 0; i < mass.length; i++){
+//     console.log(mass[i]);
+// }
+
+// for (var i = 1; i<=10; i++){
+//     console.log(i*2);
+// }
+
+
+// var ololo = null;
+//
+//
+// if (ololo === undefined) {
+//     console.log('PIZDOSH')
+// } else {
+//     console.log(typeof(ololo))
+// }
+
+
+var array = ["dima", 22, 'Suhiv', ['plan', 'bong', 'vodnyu']]; // bong
+
+
+
+
+
+// Переменная lang может принимать 2 значения: 'ru' 'en'.
+//     Если она имеет значение 'ru', то в переменную arr запишем массив дней недели на русском языке,
+//     а если имеет значение 'en' – то на английско
